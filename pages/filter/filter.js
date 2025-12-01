884778d5d412a05bf30242c8261bb6fff0c8e4f7
Bagas <bagasaldianata20@gmail.com>
2025-12-01 14:26:46 +0700

    Membuat Tes Filter Pada Halaman Pengguna

--- pages/filter/filter.js
+++ pages/filter/filter.js
@@ -3,19 +3,115 @@ import { expect } from "@playwright/test";
 export class filter{
     constructor(page){
         this.page = page;
-        this.opsiFilter = page.getByRole('button', { name: 'Filters' });
-        this.labelFilter = page.getByLabel('Filters');
-        this.inputDatePicker = page.getByRole('textbox', { name: 'Datepicker input' });
-
-
+        //Filter
+        this.buttonFilter = page.locator('#btnDropdownFilter');
+        this.dropdownFilter = page.locator('#dropdownFilter');
+        //Date Range
+        this.inputDateRange = page.locator('#inputFilterDateRange');
+        //Role
+        this.role = page.getByRole('textbox', { name: 'Role' });
+        this.dropdownRole = page.locator('#dropdownRole');
     }
 
     async filterOption(){
-        await this.opsiFilter.click();
-        await expect(this.labelFilter).toBeVisible();
+        await this.buttonFilter.click();
+        await expect(this.dropdownFilter).toBeVisible();
     }
 
-    async filterDateRange(){
-        await this.inputDatePicker.click();
+    async filterDateRange(tahunStart, bulanStart, tanggalStart, tahunEnd, bulanEnd, tanggalEnd) {
+        await this.inputDateRange.click();
+        //check muncul kalender
+        await expect(this.page.getByRole('grid', { name: 'Calendar wrapper' })).toBeVisible();
+
+        //Pilih tanggal awal
+        await this.tanggalAwal(tahunStart, bulanStart, tanggalStart);
+        //Pilih tanggal akhir
+        await this.tanggalAkhir(tahunEnd, bulanEnd, tanggalEnd);
+
+        await this.page.getByRole('button', { name: 'Confirm' }).click();
+    }
+
+    async getCurrentMonthYear() {
+        const monthBtn = this.page.getByRole('button', { name: 'Open months overlay' });
+        const yearBtn  = this.page.getByRole('button', { name: 'Open years overlay' });
+
+        const bulanSekarang = (await monthBtn.innerText()).trim();
+        const tahunSekarang = (await yearBtn.innerText()).trim();
+
+        return { bulanSekarang, tahunSekarang, monthBtn, yearBtn };
+    }
+
+    async tanggalAwal(tahun, bulan, tanggal){
+        const { bulanSekarang, tahunSekarang, monthBtn, yearBtn } = await this.getCurrentMonthYear();
+
+        const actions = [
+            {
+                current: bulanSekarang,
+                value: bulan,
+                btn: monthBtn,
+            },
+            {
+                current: tahunSekarang,
+                value: tahun,
+                btn: yearBtn,
+            }
+        ];
+
+        for (const a of actions) {
+            if (a.value !== a.current) {
+                await a.btn.click();  // buka overlay
+                await this.page
+                    .getByLabel('Filters')
+                    .getByText(a.value, { exact: true })
+                    .click();
+            }
+        }
+
+        // Select Tanggal
+        await this.page
+            .getByLabel('Filters')
+            .getByText(tanggal, { exact: true })
+            .first()
+            .click();
+    }
+
+    async tanggalAkhir(tahun, bulan, tanggal){
+        const { bulanSekarang, tahunSekarang, monthBtn, yearBtn } = await this.getCurrentMonthYear();
+
+        const actions = [
+            {
+                current: bulanSekarang,
+                value: bulan,
+                btn: monthBtn,
+            },
+            {
+                current: tahunSekarang,
+                value: tahun,
+                btn: yearBtn,
+            }
+        ];
+
+        for (const a of actions) {
+            if (a.value !== a.current) {
+                await a.btn.click();
+                await this.page
+                    .getByLabel('Filters')
+                    .getByText(a.value, { exact: true })
+                    .click();
+            }
+        }
+        // Select Tanggal
+        await this.page
+            .getByLabel('Filters')
+            .getByText(tanggal, { exact: true })
+            .first()
+            .click();
+    }
+
+    async selectRole(namaRole){
+        await this.role.fill(namaRole);
+        await expect(this.dropdownRole).toBeVisible();
+        await this.dropdownRole.getByText(namaRole, { exact: true }).click();
+        await expect(this.role).toHaveValue(namaRole);
     }
 }
--- tests/pengguna.spec.js
+++ tests/pengguna.spec.js
@@ -3,20 +3,16 @@ import { formLogin } from "../pages/login/formLogin";
 import { sidebar } from "../pages/sidebar";
 import { search } from "../pages/search/search";
 import { filter } from "../pages/filter/filter";
+import { button } from "../pages/button";
 
 test.describe('Pengguna', () => {
-    let login, sidebarClick, filterOpsi;
-
-    const runSearchTest = async (page, column, keyword) => {
-        const inputSearch = new search(page, column);
-        await inputSearch.search(keyword);
-        await inputSearch.checkSearch();
-    }
+    let login, sidebarClick, filterOpsi, buttonAction;
 
     test.beforeEach(async ({ page }) => {
             login = new formLogin(page);
             sidebarClick = new sidebar(page);
             filterOpsi = new filter(page);
+            buttonAction = new button(page);
     
             //Login
             await login.navigate('https://hse-staging.transtrack.id/login');
@@ -31,6 +27,12 @@ test.describe('Pengguna', () => {
     })
 
     test.describe('Pengguna - Search', () => {
+        const runSearchTest = async (page, column, keyword) => {
+            const inputSearch = new search(page, column);
+            await inputSearch.search(keyword);
+            await inputSearch.checkSearch();
+        }
+
         test('Search By Nama Pengguna - Search Nama Pengguna Terdata', async ({ page }) => {
             await runSearchTest(page, 1, "Bagas Intern QA");
         })
@@ -42,15 +44,64 @@ test.describe('Pengguna', () => {
         test('Search Tidak Terdata', async ({ page }) => {
             await runSearchTest(page, 1, "Farhan");
         })
+
+        test('Search By Nama Pengguna - Search Nama Pengguna Setelah Melakukan Filter Date Range', async ({ page }) => {
+            await filterOpsi.filterDateRange(
+                '2025', 'Oct', '20',
+                '2025', 'Oct', '23'
+            );
+            await buttonAction.checkAndClick('Terapkan Filter');
+            await runSearchTest(page, 1, "Bagas Intern QA");
+        })
+
+        test('Search By Nama Pengguna - Search Nama Pengguna Setelah Melakukan Filter Role', async ({ page }) => {
+            await filterOpsi.selectRole('Admin');
+            await buttonAction.checkAndClick('Terapkan Filter');
+            await runSearchTest(page, 1, "Bagas Intern QA");
+        })
+
+        test.afterEach(async ({ page }) => {
+            await page.mouse.click(50, 50);
+        })
     })
 
     test.describe('Pengguna - Filter', () => {
+        const runFIlterTest = async (page, column) => {
+            const inputSearch = new search(page, column);
+            await inputSearch.checkSearch();
+        }
+
         test.beforeEach(async ({ page }) => {
             await filterOpsi.filterOption();
         })
 
         test('Date Range', async ({ page }) => {
-            await filterOpsi.filterDateRange();
+            await filterOpsi.filterDateRange(
+                '2025', 'Oct', '20',
+                '2025', 'Oct', '23'
+            );
+            await buttonAction.checkAndClick('Terapkan Filter');
+            await runFIlterTest(page, 7);
+        })
+
+        test('Select Role', async ({ page }) => {
+            await filterOpsi.selectRole('Supervisor');
+            await buttonAction.checkAndClick('Terapkan Filter');
+            await runFIlterTest(page, 3);
+        })
+
+        test('Hapus Semua Filter', async ({ page }) => {
+            await filterOpsi.filterDateRange(
+                '2025', 'Oct', '20',
+                '2025', 'Oct', '23'
+            );
+            await filterOpsi.selectRole('Supervisor');
+            await buttonAction.checkAndClick('Reset');
+            await runFIlterTest(page, 1);
+        })
+
+        test.afterEach(async ({ page }) => {
+            await page.mouse.click(50, 50);
         })
     })
 })
